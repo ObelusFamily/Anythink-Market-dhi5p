fa5366715bdf27800fdbf4ce41689f57e4bc1acf
vidhanshu <vidhanshu7218555039@gmail.com>
2022-08-01 23:03:24 +0530

    changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import "./custom.scss";
+import App from "./components/App";
+import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import React from "react";
-import { store, history } from "./store";
-
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
-
-import App from "./components/App";
+import { history, store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,5 +16,5 @@ ReactDOM.render(
     </ConnectedRouter>
   </Provider>,
 
-  document.getElementById("root")
+  document.getElementById("app-root")
 );
